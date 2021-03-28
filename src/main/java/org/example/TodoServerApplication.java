c1349bfcc800544b29a359d3381530bd67ccacbe
Youngju-Jang <joj1043@users.noreply.github.com>
2021-03-29 08:15:29 +0900

    service code completed coding controller

---
+++ src/main/java/org/example/TodoServerApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import repository.TodoRepository;
+
+public class TodoServerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        TodoRepository repository;
+        
+    }
+}
